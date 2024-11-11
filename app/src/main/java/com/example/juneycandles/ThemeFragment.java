4f924f4014e2866a6662a434f64aa02309eb21fb
koidxueqin <p21013129@student.newinti.edu.my>
2024-11-11 15:22:45 +0800

    buh

--- app/src/main/java/com/example/juneycandles/ThemeFragment.java
+++ app/src/main/java/com/example/juneycandles/ThemeFragment.java
@@ -13,26 +13,5 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class ThemeFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_theme, container, false);
 
-        // Get the zodiacSign ImageButton and set an OnClickListener
-        ImageButton zodiacSignButton = view.findViewById(R.id.zodiacSign);
-        zodiacSignButton.setOnClickListener(v -> openZodiacFragment());
-
-        return view;
-    }
-
-    private void openZodiacFragment() {
-        // Create a new instance of ZodiacFragment
-        ZodiacFragment zodiacFragment = new ZodiacFragment();
-
-        // Replace the outerFrame in home_page.xml with ZodiacFragment
-        FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.outerFrame, zodiacFragment);
-        transaction.addToBackStack(null); // Optional: adds transaction to back stack for "back" functionality
-        transaction.commit();
-    }
 }
